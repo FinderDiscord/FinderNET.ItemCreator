b66d3c309e8fbc6aab90efd5399bfed45b0f935c
Jake Gillman <57807696+JakeyGilly@users.noreply.github.com>
2022-06-28 12:56:06 +0100

    a

---
+++ Enums/ItemRarity.cs
@@ -0,0 +1,10 @@
+namespace FinderNET.ItemCreator.Enums {
+    public enum ItemRarity {
+        Common,
+        Uncommon,
+        Rare,
+        Epic,
+        Legendary,
+        Mythic
+    }
+}
